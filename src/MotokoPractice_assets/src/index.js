b48dfcf73ef3c810ecc983e71ed1d5e88383f25a
tomkoom <tomash.sugint@gmail.com>
2021-11-30 22:39:41 +0300

    feat: install react

--- src/MotokoPractice_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { MotokoPractice } from "../../declarations/MotokoPractice";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with MotokoPractice actor, calling the greet method
-  const greeting = await MotokoPractice.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
---
+++ src/MotokoPractice_assets/src/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+import { MotokoPractice } from "../../declarations/MotokoPractice";
+
+const Index = () => {
+	return (
+		<div>
+			<p>123</p>
+		</div>
+	);
+};
+
+render(<Index />, document.getElementById("app"));
